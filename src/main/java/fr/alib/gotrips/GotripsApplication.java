9794a3785d3c0c9c8ae8720cbdad6833b750396a
ali-bennezia <bennezia.ali@gmail.com>
2024-04-30 09:25:18 +0200

    init

---
+++ src/main/java/fr/alib/gotrips/GotripsApplication.java
@@ -0,0 +1,13 @@
+package fr.alib.gotrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotripsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/alib/gotrips/GotripsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.alib.gotrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
