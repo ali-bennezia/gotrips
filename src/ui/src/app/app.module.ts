fc113d71bbdb6364788f417674b72bc7bebf54d2
ali-bennezia <bennezia.ali@gmail.com>
2024-05-09 15:00:17 +0200

    added & styled navbar, central menu item, account button

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+
+import { NgIconsModule } from '@ng-icons/core';
+import { bootstrapLuggageFill } from '@ng-icons/bootstrap-icons';
+import {
+  ionPersonCircleOutline,
+  ionAirplaneOutline,
+  ionBusinessOutline,
+  ionBaseballOutline,
+} from '@ng-icons/ionicons';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    NgIconsModule.withIcons({
+      bootstrapLuggageFill,
+      ionPersonCircleOutline,
+      ionAirplaneOutline,
+      ionBusinessOutline,
+      ionBaseballOutline,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/ui/src/app/routing/ROUTES.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+
+const ROUTES: Routes = [];
+
+export default ROUTES;
